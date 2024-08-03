80d7c937dacafbd926084168eccce65e8c5d4f67
walhalla4 <ktksg8@gmail.com>
2024-08-03 10:10:39 +0900

    Init

---
+++ src/main/java/com/flow/file/FlowFileApplication.java
@@ -0,0 +1,13 @@
+package com.flow.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flow/file/FlowFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
